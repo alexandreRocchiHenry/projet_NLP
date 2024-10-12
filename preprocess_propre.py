64cd8475e049ac3018702d936c5f862ba9b1387c
alexandreRocchiHenry <alexandrerocchi2@gmail.com>
2024-10-12 10:22:35 +0200

    preprocess

--- preprocess_propre.py
+++ preprocess_propre.py
@@ -8,6 +8,7 @@ from nltk.corpus import stopwords
 import langid
 import pandas as pd
 import re
+from bs4 import BeautifulSoup
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -59,19 +60,33 @@ metadata_df['tfidf'] = list(tfidf_matrix.toarray())
 print(metadata_df['text'].head())
 
 
+def is_css_document(text):
+    """
+    Vérifie si un document contient principalement du CSS en se basant sur la fréquence des éléments CSS.
+    """
+    # Indicateurs courants dans les blocs CSS
+    css_indicators = ['{', '}', 'margin', 'padding', 'font-size', 'color', ':root', '@media']
+    
+    # Compter combien de fois ces éléments apparaissent dans le texte
+    css_count = sum(text.count(indicator) for indicator in css_indicators)
+    
+    # Si le nombre d'éléments CSS dépasse un certain seuil par rapport à la longueur du texte, on considère que c'est du CSS
+    if css_count > 5 and css_count / len(text) > 0.05:  # Ajustez le seuil selon vos données
+        return True
+    return False
+
+# Appliquer le filtrage directement à la colonne 'text'
+metadata_df = metadata_df[metadata_df['text'].apply(is_css_document) == False]
+
+
 def preprocess_text(text):
     text = text.lower()
 
-    text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
-    text = re.sub(r'\S+@\S+', '', text)
+    text = re.sub(r'(#+\S+|@\S+|\s+@\S+|http\S+|word01|word02|[^A-Za-z0-9\'\’ ]+)', "", text)
 
-    text = re.sub(r'[^a-zA-Z\s]', '', text)
-    
-    text = re.sub(r'\d+\s*[\$€£]\s*\d+', '', text) 
-    text = re.sub(r'©\s*\d{4}', '', text) 
 
-    
-    text = re.sub(r'[\n\t]+', ' ', text)   # Remplacer les retours 
+    text = re.sub(r'[\n\t]+', ' ', text)
+    text = re.sub(r'[\n\r]+', ' ', text)
     text = re.sub(r'\s+', ' ', text)  # Remplacer les espaces multiples
 
 
@@ -79,10 +94,10 @@ def preprocess_text(text):
     cleaned_text = ' '.join(tokens)
     
     return cleaned_text
-
+print('preprocessing')
 metadata_df['text_processed'] = metadata_df['text'].apply(preprocess_text)
 print('\npreprocessed\n')
 print(metadata_df['text_processed'].head(5))
-
+print('preprocessed, writning csv')
 
 metadata_df.to_csv('data_preprocessed.csv', index=False)
